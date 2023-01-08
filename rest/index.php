bf4f81da489e83bc5e8c18437f505dfee2369022
enesekremergunesh <enes.ekrem.ergunes@stu.ibu.edu.ba>
2023-01-08 11:29:53 +0100

    initial commit

--- rest/index.php
+++ rest/index.php
@@ -3,37 +3,64 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once 'dao/ChatDao.class.php';
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
-require_once '../vendor/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/services/NoteService.class.php';
+require_once __DIR__.'/services/TodoService.class.php';
+require_once __DIR__.'/dao/UserDao.class.php';
 
-// Flight::register('chatDao', 'ChatDao');
-$chatDao = new ChatDao();
+Flight::register('userDao', 'UserDao');
+Flight::register('todoService', 'TodoService');
+Flight::register('noteService', 'NoteService');
 
-Flight::route('GET /messages', function(){ 
-Flight::json(Flight::chatDao()->get_all());
+Flight::map('error', function(Exception $ex){
+    // Handle error
+    Flight::json(['message' => $ex->getMessage()], 500);
 });
 
-    Flight::route('GET /messages/@id', function($id){
-        Flight::json(Flight::chatDao()->get_by_id($id));
-        });
+/* utility function for reading query parameters from URL */
+Flight::map('query', function($name, $default_value = NULL){
+  $request = Flight::request();
+  $query_param = @$request->query->getData()[$name];
+  $query_param = $query_param ? $query_param : $default_value;
+  return urldecode($query_param);
+});
 
-Flight::route('POST /messages', function(){ // i deleted $id from the parameter
-    Flight::json(Flight::chatDao()->add(Flight::request()->data->getData()));
-    });
+// middleware method for login
+Flight::route('/*', function(){
+  //return TRUE;
+  //perform JWT decode
+  $path = Flight::request()->url;
+  if ($path == '/login' || $path == '/docs.json') return TRUE; // exclude login route from middleware
 
-    Flight::route('PUT/messages/@id', function($id){
-        $data = Flight::request()->data->getData();
-        $data['id'] = $id;
-    Flight::json(Flight::chatDao()->update($data));
-    });
+  $headers = getallheaders();
+  if (@!$headers['Authorization']){
+    Flight::json(["message" => "Authorization is missing"], 403);
+    return FALSE;
+  }else{
+    try {
+      $decoded = (array)JWT::decode($headers['Authorization'], new Key(Config::JWT_SECRET(), 'HS256'));
+      Flight::set('user', $decoded);
+      return TRUE;
+    } catch (\Exception $e) {
+      Flight::json(["message" => "Authorization token is not valid"], 403);
+      return FALSE;
+    }
+  }
+});
 
-    Flight::route('DELETE/people/@id', function($id){
-        Flight::chatDao()->delete($id);
-        Flight::json(["message" => "deleted"]);
-        });
+/* REST API documentation endpoint */
+Flight::route('GET /docs.json', function(){
+  $openapi = \OpenApi\scan('routes');
+  header('Content-Type: application/json');
+  echo $openapi->toJson();
+});
 
-    Flight::start();
+require_once __DIR__.'/routes/TodoRoutes.php';
+require_once __DIR__.'/routes/NoteRoutes.php';
+require_once __DIR__.'/routes/UserRoutes.php';
 
-
+Flight::start();
 ?>
